8d2b831ccc1da6aadbf89b911082e30d746a7ded
Kerem Nalbant <kerem.nalbant@hotmail.com>
2023-07-04 02:44:46 +0300

    comment: add comments to subscriber.ts

--- src/utils/subscriber.ts
+++ src/utils/subscriber.ts
@@ -1,6 +1,21 @@
-/* Actually this pattern (event bus) is not that necessary,
-but it is a good example of how to use the event emitter in typescript.
+/* This pattern (Event Bus) is not that necessary in this type of project,
+but it is a good example of how to use the EventEmitter in React/TypeScript.
+
+The idea is to create a global event emitter that can be used to subscribe
+and publish events across the application. This is useful when you have
+components that are not directly related to each other, but you still need
+to communicate between them.
+
+For example, you can use this to communicate between a component and a service
+that is not directly related to it.
+
+For Microfrontends, instead of using node's EventEmitter, you can use CustomEvent,
+and listen by window.addEventListener and dispatch by window.dispatchEvent
+
+Error handling may be necessary in some cases, but it is not implemented here.
 */
+
+//TODO: remove this file in future
 
 import { EventEmitter } from 'events';
 import { EventName } from '../models';
